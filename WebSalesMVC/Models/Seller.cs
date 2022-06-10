c933533d84e74f8998028b018d3335186212f567
EmanueleRangel <emanuele.rangel52@gmail.com>
2022-06-09 23:31:49 -0300

    correct referential integrity

--- WebSalesMVC/Models/Seller.cs
+++ WebSalesMVC/Models/Seller.cs
@@ -10,6 +10,7 @@ namespace WebSalesMVC.Models {
     public DateTime BirthDate { get; set; }
     public double BaseSalary { get; set; }
     public Department Department { get; set; }
+    public int DepartmentId { get; set; }
     public ICollection<SaleRecord> Sales { get; set; } = new List<SaleRecord>();
 
     public Seller() { }
